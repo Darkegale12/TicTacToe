d7f8a1c827e2929a19ce20bafaf19975ad210af7
Vishwajeet More <vishwajeetmore2023.comp@mmcoe.edu.in>
2024-09-05 23:04:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+const cells = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessageTextElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let isCircleTurn;
+
+function startGame() {
+    isCircleTurn = false;
+    cells.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('circle');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageTextElement.textContent = "";
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = isCircleTurn ? 'circle' : 'x';
+    placeMark(cell, currentClass);
+
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    isCircleTurn = !isCircleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove('x');
+    board.classList.remove('circle');
+    if (isCircleTurn) {
+        board.classList.add('circle');
+    } else {
+        board.classList.add('x');
+    }
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains('x') || cell.classList.contains('circle');
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.textContent = "It's a Draw!";
+    } else {
+        winningMessageTextElement.textContent = `${isCircleTurn ? "O's" : "X's"} Win!`;
+    }
+}
+
+restartButton.addEventListener('click', startGame);
+
+startGame();
